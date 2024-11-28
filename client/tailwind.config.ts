a8412c56e119f11452dd4c54a45b2b24ce701944
Krishnas-world <krishnapallan128@gmail.com>
2024-11-28 16:34:20 +0530

    feature: added the shadCN and created home page

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,18 +1,35 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			customColor: {
+  				'1': '#fdfefe',
+  				'2': '#2b4464',
+  				'3': '#b6cfd6',
+  				'4': '#173257',
+  				'5': '#6da8b0',
+  				'6': '#173255'
+  			}
+  		},
+  		animation: {
+  			'up-down': 'up-down 2s ease-in-out infinite alternate'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
